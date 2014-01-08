f42171b2fb74bbf49e0ace6ebcb4852521860d4d
foyrob <rob.foy@gmail.com>
2014-01-08 09:51:55 +0000

    Created function for adding two numbers

---
+++ weatherbasic.R
@@ -0,0 +1,12 @@
+w = read.table('~/Dropbox/PhD/weather/daily-text/1999_01_19', header = FALSE, sep = "\t")w
+summary(w)
+
+hd <- readLines(f)
+hd <- hd[grep("#", hd)]
+hd <- sub("#", "", hd)
+hd <- hd[7:8]
+hd <- gsub(" ", "", hd)
+hd <- strsplit(hd, "\t")
+hd <- paste0(hd[[1]], " [", hd[[2]], "]")
+hd <- sub(" \\[\\]", "", hd)
+names(w) <- hd
